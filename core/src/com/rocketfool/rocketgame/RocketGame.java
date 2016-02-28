7ea7b0eaaf6fb7bc6150a7193827db7b2b1acc34
PythEch <pythech.tr@gmail.com>
2016-02-28 20:30:30 +0200

    Draw background and player

--- core/src/com/rocketfool/rocketgame/RocketGame.java
+++ core/src/com/rocketfool/rocketgame/RocketGame.java
@@ -3,32 +3,43 @@ package com.rocketfool.rocketgame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class RocketGame extends ApplicationAdapter {
-	public static final String VERSION = "0.1";
-	public static final String TITLE = "Rocket Game";
+    public static final String VERSION = "0.1";
+    public static final String TITLE = "Rocket Game";
 
-	public static final int WIDTH = 1280;
+    public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
-    public static final int SCALE = 1;
 
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    private Texture playerImage;
+    private Texture backgroundImage;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        playerImage = new Texture("PNG/playerShip2_orange.png");
+        backgroundImage = new Texture("Backgrounds/darkPurple.png");
+        // makes background repeatable
+        backgroundImage.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // This cryptic line clears the screen.
+        batch.begin();
+        // Drawing goes here!
+
+        // repeat background
+        batch.draw(backgroundImage, 0, 0, 0, 0, WIDTH, HEIGHT);
+        // scale spaceship by 0.5
+        batch.draw(playerImage, WIDTH / 2, HEIGHT / 2, playerImage.getWidth() / 2, playerImage.getHeight() / 2);
+
+        batch.end();
+    }
 }
