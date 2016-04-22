33d52b1637024a3ccf886874357c52a089e5610c
omiomer <omer.ekmekcioglu@gmail.com>
2016-04-22 11:25:35 +0300

    intro to round obstacle

--- src/core/src/com/rocketfool/rocketgame/model/RoundObstacle.java
+++ src/core/src/com/rocketfool/rocketgame/model/RoundObstacle.java
@@ -1,6 +1,6 @@
 package com.rocketfool.rocketgame.model;
 
-import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.*;
 
 /**
  * Class for all objects with physical properties and a circular shape. Eg. asteroids.
@@ -11,6 +11,26 @@ public class RoundObstacle extends SolidObject {
     }
 
     private Body createBody(float x, float y, int radius, int speed, float angle) {
+       /* BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.KinematicBody;
+        bodyDef.position.set(x, y);
+
+        //TODO: world isnt valid as variable
+        Body body = world.createBody(bodyDef);
+
+        //initial design for the round obstancle
+        CircleShape circle = new CircleShape();
+        circle.setRadius(radius);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = circle;
+        fixtureDef.restitution = 0.0f;
+
+        body.createFixture(fixtureDef);
+
+        circle.dispose();
+        return body;
+        */
         return null;
     }
 
