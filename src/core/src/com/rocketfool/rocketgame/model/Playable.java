89005d3db32e6fe4e778897d43d85bb9d378206e
PythEch <pythech.tr@gmail.com>
2016-04-29 13:46:46 +0300

    fixxx

--- src/core/src/com/rocketfool/rocketgame/model/Playable.java
+++ src/core/src/com/rocketfool/rocketgame/model/Playable.java
@@ -115,7 +115,7 @@ public class Playable extends SolidObject {
     /** Reduce mass of the spacecraft by burning its fuel */
     private void consumeFuelAndDecreaseMass(float deltaTime) {
         if (fuelLeft > 0) {
-            float fuelSpent = currentThrust / fuelSpecificImpulse /60; //temp fix to last until the merge
+            float fuelSpent = currentThrust / fuelSpecificImpulse;
             fuelLeft -= fuelSpent;
 
             //The mass information of the body changes only when MassData is updated
