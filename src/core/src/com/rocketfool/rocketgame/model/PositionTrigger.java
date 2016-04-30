f13e168f9b630daae8453bc344d058f24b358c4f
PythEch <pythech.tr@gmail.com>
2016-05-01 00:34:23 +0300

    rip me

--- src/core/src/com/rocketfool/rocketgame/model/PositionTrigger.java
+++ src/core/src/com/rocketfool/rocketgame/model/PositionTrigger.java
@@ -10,36 +10,54 @@ public class PositionTrigger extends Trigger {
     private float x;
     private float y;
     private float radius;
-    /** When the target reaches the trigger, it is activated.*/
+    /**
+     * When the target reaches the trigger, it is activated.
+     */
     private SolidObject target;
-    /** Position triggers can be made to move in unison with designated host objects.*/
+    /**
+     * Position triggers can be made to move in unison with designated host objects.
+     */
     private SolidObject host;
     private boolean reverse;
     //endregion
 
     //region Constructors
-    public PositionTrigger(float x, float y, float radius, SolidObject target) {
+    public PositionTrigger(boolean runOnce, SolidObject host, float x, float y, float radius, SolidObject target, boolean reverse) {
+        super(runOnce);
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.target = target;
-        this.host = null;
-        this.reverse = false;
+        this.host = host;
+        this.reverse = reverse;
     }
 
-    public PositionTrigger(SolidObject host, float xOffset, float yOffset, float radius, SolidObject target) {
-        this(xOffset, yOffset, radius, target);
-        this.host = host;
+    public PositionTrigger(boolean runOnce, float x, float y, float radius, SolidObject target, boolean reverse) {
+        this(runOnce, null, x, y, radius, target, reverse);
+    }
+
+    public PositionTrigger(boolean runOnce, SolidObject host, float x, float y, float radius, SolidObject target) {
+        this(runOnce, host, x, y, radius, target, false);
+    }
+
+    public PositionTrigger(boolean runOnce, float x, float y, float radius, SolidObject target) {
+        this(runOnce, null, x, y, radius, target, false);
+    }
+
+    public PositionTrigger(SolidObject host, float x, float y, float radius, SolidObject target, boolean reverse) {
+        this(true, host, x, y, radius, target, reverse);
     }
 
     public PositionTrigger(float x, float y, float radius, SolidObject target, boolean reverse) {
-        this(x, y, radius, target);
-        this.reverse = reverse;
+        this(true, null, x, y, radius, target, reverse);
     }
 
-    public PositionTrigger(SolidObject host, float xOffset, float yOffset, float radius, SolidObject target, boolean reverse) {
-        this(host, xOffset, yOffset, radius, target);
-        this.reverse = reverse;
+    public PositionTrigger(SolidObject host, float x, float y, float radius, SolidObject target) {
+        this(true, host, x, y, radius, target, false);
+    }
+
+    public PositionTrigger(float x, float y, float radius, SolidObject target) {
+        this(true, null, x, y, radius, target, false);
     }
 
     //endregion
@@ -74,12 +92,15 @@ public class PositionTrigger extends Trigger {
     }
 
     public Vector2 getPosition() {
-        if (host == null)  {
+        if (host == null) {
             return new Vector2(x, y);
         }
         return new Vector2(x + host.getBody().getPosition().x, y + host.getBody().getPosition().y);
     }
-    public float getRadius() {return radius;}
+
+    public float getRadius() {
+        return radius;
+    }
 
     //endregion
 }
--- src/core/src/com/rocketfool/rocketgame/model/Trigger.java
+++ src/core/src/com/rocketfool/rocketgame/model/Trigger.java
@@ -4,8 +4,18 @@ package com.rocketfool.rocketgame.model;
  * Triggers are like listeners; they check for certains events/changes and perform actions.
  */
 public abstract class Trigger {
-    protected boolean runOnce = false;
-    protected boolean isTriggeredBefore = false;
+    protected boolean runOnce;
+    protected boolean isTriggeredBefore;
+
+    public Trigger(boolean runOnce) {
+        this.runOnce = runOnce;
+        this.isTriggeredBefore = false;
+    }
+
+    public Trigger() {
+        this.runOnce = true;
+        this.isTriggeredBefore = false;
+    }
 
     public boolean isTriggered() {
         if (runOnce && isTriggeredBefore) {
@@ -20,6 +30,7 @@ public abstract class Trigger {
     }
 
     protected abstract boolean isTriggeredInternal();
+
     public abstract void triggerPerformed();
 
     public boolean isRunOnce() {
